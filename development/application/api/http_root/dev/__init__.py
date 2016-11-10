f65bf16582824970b0832074159a4edee0054501
Gerold Penz <gerold@halvar.at>
2016-11-10 22:14:40 +0100

    ...

--- development/application/api/http_root/dev/__init__.py
+++ development/application/api/http_root/dev/__init__.py
@@ -70,46 +70,46 @@ jsonrpc.exposed = True
 
 def _do_iteration():
 
-    # index = 0
-    #
-    # for address in Address.query(Address.tag_items == u"Gelöscht"):
-    #     assert isinstance(address, Address)
-    #
-    #     # Log
-    #     index += 1
-    #     if index % 100 == 0:
-    #         logging.info("do_iteration: {index}".format(index = index))
-    #
-    #     # Nur geänderte Daten speichern
-    #     changed = False
-    #
-    #
-    #     if not address.category_items:
-    #         address.category_items = []
-    #     address.category_items.append(u"Kunde inaktiv")
-    #     address.tag_items.remove(u"Gelöscht")
-    #     changed = True
-    #
-    #
-    #     # if address.category_items and u"Immoads" in (address.category_items or []):
-    #     #     address.category_items.append(u"Makler")
-    #     #     address.category_items.remove(u"Immoads")
-    #     #     changed = True
-    #     #
-    #     # if address.tag_items and u"Kunde nein" in (address.tag_items or []):
-    #     #     if not address.category_items:
-    #     #         address.category_items = []
-    #     #     address.category_items.append(u"Kunde nein")
-    #     #     address.tag_items.remove(u"Kunde nein")
-    #     #     changed = True
-    #     #
-    #     # if address.tag_items and u"Imabis 2016-11" in (address.tag_items or []):
-    #     #     address.tag_items.remove(u"Imabis 2016-11")
-    #     #     changed = True
-    #
-    #     # Speichern
-    #     if changed:
-    #         address.put()
+    index = 0
+
+    for address in Address.query(Address.category_items == u"Kunde nein"):
+        assert isinstance(address, Address)
+
+        # Log
+        index += 1
+        if index % 100 == 0:
+            logging.info("do_iteration: {index}".format(index = index))
+
+        # Nur geänderte Daten speichern
+        changed = False
+
+
+        if not address.category_items:
+            address.category_items = []
+        address.category_items.append(u"Kunde-nein")
+        address.category_items.remove(u"Kunde nein")
+        changed = True
+
+
+        # if address.category_items and u"Immoads" in (address.category_items or []):
+        #     address.category_items.append(u"Makler")
+        #     address.category_items.remove(u"Immoads")
+        #     changed = True
+        #
+        # if address.tag_items and u"Kunde nein" in (address.tag_items or []):
+        #     if not address.category_items:
+        #         address.category_items = []
+        #     address.category_items.append(u"Kunde nein")
+        #     address.tag_items.remove(u"Kunde nein")
+        #     changed = True
+        #
+        # if address.tag_items and u"Imabis 2016-11" in (address.tag_items or []):
+        #     address.tag_items.remove(u"Imabis 2016-11")
+        #     changed = True
+
+        # Speichern
+        if changed:
+            address.put()
 
     logging.info("Fertig")
 
